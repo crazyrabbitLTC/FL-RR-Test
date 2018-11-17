8db85bf29f03bd16ecb9145e3b148b0b0b245f4f
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-17 16:34:55 -0500

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router , Route, Link} from "react-router-dom";
+import Child from "./components/BrowserBar";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+        <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+      <Route path="/address/:id" component={Child} />
+      <Route path="/block/:id" component={Child} />
+
+        {/* <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} /> */}
       </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
+
     <Provider store={configureStore()}>
+
      <App />
+
     </Provider>,
     document.getElementById('root')
    );
